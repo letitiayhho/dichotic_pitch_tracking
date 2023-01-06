24ac599a0158afe46748a4cd276161c3ac70b2db
Letitia Ho <letitiayhho@gmail.com>
2023-01-06 09:04:54 -0600

    Add sound testing script

---
+++ task/test_play_wav.py
@@ -0,0 +1,23 @@
+from psychopy import prefs
+prefs.hardware['audioLib'] = ['ptb']
+from psychopy.sound.backend_ptb import SoundPTB as Sound
+from psychtoolbox import GetSecs, WaitSecs
+from events import EventMarker
+from functions import *
+import RTBox
+import random
+
+# MARKER = EventMarker()
+MARKER = None
+
+
+# tone_fname = 'task/tones/test_music.wav'
+tone_fname = "task/tones/left_280.wav"
+
+# n_tones = 20
+# for i = range(n_tones):
+# rt = play_tone(MARKER, tone_fname)
+
+snd = Sound(200, secs = 10)
+now = GetSecs()
+snd.play(when = now + 0.001)
