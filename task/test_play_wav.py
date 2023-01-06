bcba41c8b308ab25fac1b60307345567ae193189
Letitia Ho <letitiayhho@uchicago.edu>
2023-01-06 09:18:33 -0600

    Play a .wav file successfully

--- task/test_play_wav.py
+++ task/test_play_wav.py
@@ -2,6 +2,7 @@ from psychopy import prefs
 prefs.hardware['audioLib'] = ['ptb']
 from psychopy.sound.backend_ptb import SoundPTB as Sound
 from psychtoolbox import GetSecs, WaitSecs
+from functions import *
 
 MARKER = None
 # tone_fname = 'task/tones/test_music.wav'
@@ -9,8 +10,10 @@ tone_fname = "task/tones/left_280.wav"
 
 # n_tones = 20
 # for i = range(n_tones):
-# rt = play_tone(MARKER, tone_fname)
+rt = play_tone(MARKER, tone_fname)
 
-snd = Sound(200, secs = 10)
-snd.play()
+# snd = Sound(200, secs = 10)
+# snd.play()
+
+
 WaitSecs(10)
