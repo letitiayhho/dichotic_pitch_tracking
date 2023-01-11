147f39695378460a2e3a168d45cc9e91df731ab4
John <johnv@uchicago.edu>
2023-01-11 14:00:16 -0600

    Fix timing test

--- task/timing_test.py
+++ task/timing_test.py
@@ -1,15 +1,26 @@
 from events import EventMarker
 from functions import *
-import RTBox
+#import RTBox
 import random
 
-# MARKER = EventMarker()
-MARKER = None
-n_tones = 20
+MARKER = EventMarker()
+#MARKER = None
+n_tones = 1000
+
+stream = get_stream()
+target = get_target()
 
 tones = get_tone_array(target)
-tone_array = tones[random.randint(0, 9)]
+marks = get_mark(tones)
+tone_id = get_is_target(tones, stream, target)
+tone_array = tones[random.randint(0, 8)]
 tone_fname = get_tone_fname(tone_array)
+weights = get_tone_weights(stream, target, tones, False)
+no_target_weights = get_tone_weights(stream, target, tones, True)
+cannot_be_target = False
 
-for i = range(n_tones):
-    rt = play_tone(MARKER, tone_fname)
+tone, is_target, mark = get_tone(tones, tone_id, marks, weights, no_target_weights, cannot_be_target)
+
+for i in range(n_tones):
+    print(i)
+    rt = play_tone(MARKER, tone_fname, mark)
