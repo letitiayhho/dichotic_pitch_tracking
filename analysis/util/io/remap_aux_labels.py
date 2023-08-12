508686d7d1b4a8a2c832e230b1d7e8b62bcd802a
Letitia Ho <letitiayhho@midway2-login1.rcc.local>
2023-08-12 13:11:42 -0500

    Fix column labels

--- analysis/util/io/remap_aux_labels.py
+++ analysis/util/io/remap_aux_labels.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def remap_aux_labels(sub, raw, remapping_fpath):
     remapping = pd.read_csv(remapping_fpath)
-    remap = remapping[(remapping['sub'] == int(sub)) & (remapping['tag'] == 12)]
-    aux = list(remap['aux'])
-    rename = list(remap['rename'])
+    sub_remap = remapping[(remapping['sub'] == int(sub))]
+    aux = list(sub_remap['aux'])
+    remap = list(sub_remap['remap'])
     remap_dict = {}
-    remap_dict[aux[0]] = rename[0]
-    remap_dict[aux[1]] = rename[1]
+    remap_dict[aux[0]] = remap[0]
+    remap_dict[aux[1]] = remap[1]
     return raw, remap_dict
